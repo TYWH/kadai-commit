c4262b34dc81db6c563d8cb50a961f8df9842920
TYWT <47287957+TYWH@users.noreply.github.com>
2019-02-03 09:20:57 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,7 @@
+<?php
+    $result = "無し";
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -7,5 +11,8 @@
     <body>
         <h1>はじめてのバージョン管理</h1>
         <p>Gitを使ってバージョン管理する。</p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
+        </form>
     </body>
 </html>
